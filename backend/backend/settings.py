b5c6ac5f230ec6ae0b43415a305fb19f0a0e04a4
Alexey Pakaev <alexey1917@gmail.com>
2024-05-30 04:42:05 +0300

    version

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,9 +62,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': '/data/db.sqlite3',
-	'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
